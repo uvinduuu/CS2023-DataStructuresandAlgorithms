10d4ff1a817501c65ea293bfe49f8d39f97d1feb
Uvindu Kodikara <153884375+uvinduuu@users.noreply.github.com>
2024-06-11 00:58:17 +0530

    Create Lab1_3.cpp

---
+++ Labs/Lab01/Lab1_3.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int a, b, c; //initiating integar variables for inputs
+    cin>>a>>b>>c; //getting the inputs
+    int sum=a+b+c; //calculating the sum
+    cout<<sum<<endl; //outputs the sum
+    return 0;
+}
